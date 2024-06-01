bd7ca1f1f5d401f74e308d3993185c9b66b5f789
peixed351 <peixed351@gmail.com>
2024-06-01 18:43:32 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/Amir4g/CLVOS23"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 16585415
+PREVIEW_IMAGE_ID: int = 16589439
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/clvos23"
